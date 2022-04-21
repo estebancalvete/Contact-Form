61c92bfc38ee84b96bae6ba494b3f537ce377cd7
Esteban Calvete <esteban.calvete@gmail.com>
2022-04-21 10:20:00 +0200

    Initial Commit

---
+++ Contact Form/Contact_FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Contact_FormApp.swift
+//  Contact Form
+//
+//  Created by Esteban Calvete Iglesias on 21/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Contact_FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Contact Form/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Contact Form
+//
+//  Created by Esteban Calvete Iglesias on 21/4/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
