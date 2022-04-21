91e8cf144977d42c4829b2aa6c2acaaa09bf8d9b
Esteban Calvete <esteban.calvete@gmail.com>
2022-04-21 14:55:23 +0200

    First commit

--- Contact Form/ContentView.swift
+++ Contact Form/ContentView.swift
@@ -8,10 +8,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var emailAddress = ""
+    @State private var phoneNumber = ""
+    @State private var message = "Write your message here..."
+    @State private var includeLogs = false
+    @State private var subject = "Help"
+    @State private var preferredContactMethod = ContactMethod.email
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                Section(header: Text("How can we reach you?")) {
+                    Picker("Contact Method", selection: $preferredContactMethod) {
+                        Text("Email").tag(ContactMethod.email)
+                        Text("Phone").tag(ContactMethod.phone)
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                    switch preferredContactMethod {
+                    case .email:
+                        TextField("Email Address", text: $emailAddress)
+                    case .phone:
+                        TextField("Phone", text: $phoneNumber)
+                    }
+                }
+                Section(header: Text("How can we help you with?")) {
+                    Picker("Subject", selection: $subject) {
+                        ForEach(["Help", "Suggestion", "Report Bug"], id: \.self) { subject in
+                            Text(subject)
+                        }
+                    }
+                }
+                Section(header: Text("Briefly explain what's going on.")) {
+                    TextEditor(text: $message)
+                }
+                Section(footer: Text("This information will be sent anonymously.")) {
+                    Toggle("Include Logs", isOn: $includeLogs)
+                }
+                Button("Submit", action: {
+                    print("Submit button tapped")
+                })
+            }
+            .navigationTitle("Contact Us")
+        }
     }
+}
+
+enum ContactMethod {
+    case email, phone
 }
 
 struct ContentView_Previews: PreviewProvider {
